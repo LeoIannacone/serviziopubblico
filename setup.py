1d769790244188f536d8cb0e07bb51603a98d758
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-30 14:25:07 +0200

    release v0.2

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-import os, sys
 from distutils.core import setup
 
 setup(
-    name = "Servizio Pubblico",
-    version = "0.1",
-    author = "Leo Iannacone",
-    author_email = "l3on@ubuntu.com",
-    description = ("Un semplice script scaricare le puntate di Servizio Pubblico"),
-    license = "GPLv3",
-    keywords = "video download",
-    url = "https://github.com/LeoIannacone/serviziopubblico",
+    name="Servizio Pubblico",
+    version="0.2",
+    author="Leo Iannacone",
+    author_email="l3on@ubuntu.com",
+    description=("Un semplice script scaricare le puntate di Servizio Pubblico"),
+    license="GPLv3",
+    keywords="video download",
+    url="https://github.com/LeoIannacone/serviziopubblico",
     #long_description=read('README.md'),
-    
-    scripts = ['serviziopubblico/serviziopubblico'],
-    
+
+    scripts=['serviziopubblico/serviziopubblico'],
+
     data_files=[
         ('share/serviziopubblico', ['README.md', ]), # 'Changelog', 'Authors']),
         ('share/man/man1', ['man/serviziopubblico.1']),
